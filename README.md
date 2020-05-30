576d1c349a84917b29d6cfb29c9fa0352c56b6fc
Amrit <amrit2101@gmail.com>
2020-05-31 02:17:55 +0530

    Inserting values into database

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ sudo mysql -u root
 mysql> CREATE USER 'aish'@'localhost' IDENTIFIED BY 'aish';
 mysql> GRANT ALL PRIVILEGES ON _ . _ TO 'aish'@'localhost';
 npm install mysql
+npm install express
+npm install body-parser multer
--- myserver.js
+++ myserver.js
@@ -1,6 +1,10 @@
 var http = require("http");
 var fs = require("fs");
 var mysql = require("mysql");
+var express = require("express");
+var bodyParser = require("body-parser");
+var multer = require("multer");
+var upload = multer();
 const DB_NAME = "students";
 const TABLE_NAME = "profile";
 
@@ -41,7 +45,7 @@ function createTable() {
     con.query(
       "CREATE TABLE IF NOT EXISTS " +
         TABLE_NAME +
-        "(name varchar(50), age TINYINT, about varchar(300))",
+        "(id int NOT NULL PRIMARY KEY AUTO_INCREMENT, name varchar(50), age TINYINT, about varchar(300))",
       function(err, result) {
         if (err) throw err;
         console.log("Table created");
@@ -52,12 +56,64 @@ function createTable() {
 
 createDatabaseAndTable();
 
-http
-  .createServer(function(req, res) {
-    fs.readFile("formpage.html", function(err, data) {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
-  })
-  .listen(8081);
+// http
+//   .createServer(function(req, res) {
+//     console.log(req);
+//     fs.readFile("formpage.html", function(err, data) {
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.write(data);
+//       return res.end();
+//     });
+//   })
+//   .listen(8081);
+
+var app = express();
+
+app.get("/", function(req, res) {
+  fs.readFile("formpage.html", function(err, data) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+    return res.end();
+  });
+});
+
+app.get("/xyz", function(req, res) {
+  res.send("xyz path");
+});
+
+app.get("/aish", function(req, res) {
+  res.send("aish path");
+});
+
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+//form-urlencoded
+
+// for parsing multipart/form-data
+app.use(upload.array());
+app.use(express.static("public"));
+
+app.post("/createstudent", function(req, res) {
+  console.log(req.body);
+  con.query(
+    "INSERT INTO " +
+      TABLE_NAME +
+      "(name, age, about) VALUES ('" +
+      req.body.sname +
+      "'," +
+      req.body.sage +
+      ",'" +
+      req.body.sdesc +
+      "');",
+    function(err, result) {
+      if (err) throw err;
+      console.log("Inserted entry");
+      res.send("Inserted");
+    }
+  );
+});
+
+app.listen(8081);
