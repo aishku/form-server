23e0a9cf2fac8cd09e3909b084426b543610ec98
Amrit <amrit2101@gmail.com>
2020-05-31 00:29:20 +0530

    Connecting to mySQL

--- myserver.js
+++ myserver.js
@@ -1,5 +1,56 @@
 var http = require("http");
 var fs = require("fs");
+var mysql = require("mysql");
+const DB_NAME = "students";
+const TABLE_NAME = "profile";
+
+var con;
+
+function createDatabaseAndTable() {
+  con = mysql.createConnection({
+    host: "localhost",
+    user: "aish",
+    password: "aish"
+  });
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE IF NOT EXISTS " + DB_NAME, function(
+      err,
+      result
+    ) {
+      if (err) throw err;
+      console.log("Database created");
+      con.end();
+      console.log("Disconnected");
+      createTable();
+    });
+  });
+}
+
+function createTable() {
+  con = mysql.createConnection({
+    host: "localhost",
+    user: "aish",
+    password: "aish",
+    database: DB_NAME
+  });
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected Again!");
+    con.query(
+      "CREATE TABLE IF NOT EXISTS " +
+        TABLE_NAME +
+        "(name varchar(50), age TINYINT, about varchar(300))",
+      function(err, result) {
+        if (err) throw err;
+        console.log("Table created");
+      }
+    );
+  });
+}
+
+createDatabaseAndTable();
 
 http
   .createServer(function(req, res) {
